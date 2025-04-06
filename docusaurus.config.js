58d569f8a13c36a5da24b741d33076050b2fb3e9
Adam Hakarsa <adamnoto@hey.com>
2025-04-07 07:11:58 +0900

    Add basic IDE page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: 'https://kelas.kodegara.org/', label: 'Kelas (IDE)', position: 'left'},
+        {to: 'pathname:///ide.html', label: 'Kelas (IDE)', position: 'left'},
         {to: 'blog', label: 'Blog', position: 'left'},
       ],
     },
